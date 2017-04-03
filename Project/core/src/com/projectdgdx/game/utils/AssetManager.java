0269de6ccb5aecfd3be2eafef87ca652354c7fac
Konglobemeralt <jesper.blidkvist@live.se>
2017-04-03 14:40:12 +0200

    added functions for returning instances of models

--- Project/core/src/com/projectdgdx/game/utils/AssetManager.java
+++ Project/core/src/com/projectdgdx/game/utils/AssetManager.java
@@ -1,10 +1,13 @@
 package com.projectdgdx.game.utils;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.loaders.TextureLoader;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by konglobemeralt on 2017-04-03.
@@ -14,10 +17,17 @@ public class AssetManager {
     private ModelBatch modelBatch;
     private com.badlogic.gdx.assets.AssetManager rawAssets;
 
+    private TextureLoader.TextureParameter param;
+
+    private Array<String> loadedModels = new Array<String>();
+
     public AssetManager(){
         modelBatch = new ModelBatch();
         rawAssets = new com.badlogic.gdx.assets.AssetManager();
 
+        param = new TextureLoader.TextureParameter();
+        param.minFilter = Texture.TextureFilter.Linear;
+        param.genMipMaps = true;
     }
 
     public void loadModel(String model){
@@ -25,15 +35,26 @@ public class AssetManager {
     }
 
     public void loadTexture(String texture){
-        rawAssets.load(texture, Texture.class);
+        rawAssets.load(texture, Texture.class, param);
     }
 
     public void setTextureToModel(String textureToSet, String modelToChange){
         Model model = rawAssets.get(modelToChange, Model.class);
-        Texture newModelTexture = new Texture(Gdx.files.internal("copper.jpg"));
+        Texture newModelTexture = new Texture(Gdx.files.internal(textureToSet));
+
         //In the case that we use complex with a number of nodes this will obviously need to be updated to change
-        // a specific node instead of the root node.
+        // a specific node instead of the root node 0.
         model.materials.get(0).set(TextureAttribute.createDiffuse(newModelTexture));
+    }
+
+    public ModelInstance getModel(String modelString){
+        if(!checkIfLoaded(modelString)){
+            loadModel(modelString);
+        }
+
+        Model model = rawAssets.get(modelString, Model.class);
+        ModelInstance modelInstance = new ModelInstance(model);
+        return modelInstance;
     }
 
     public void dispose(){
@@ -41,5 +62,12 @@ public class AssetManager {
         modelBatch.dispose();
     }
 
-
+    private boolean checkIfLoaded(String assetToCheck){
+        for(String model : loadedModels) {
+            if (assetToCheck.equals(model)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
