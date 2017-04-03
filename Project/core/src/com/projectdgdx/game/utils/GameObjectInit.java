5e7026ea2a41bcaedb85ab65bd9b68e68b30077b
Zumpo <carlsson889@gmail.com>
2017-04-03 10:38:52 +0200

    Fixed errors in GameObjectInit

--- Project/core/src/com/projectdgdx/game/utils/GameObjectInit.java
+++ Project/core/src/com/projectdgdx/game/utils/GameObjectInit.java
@@ -1,6 +1,8 @@
 package com.projectdgdx.game.utils;
 
+import com.badlogic.gdx.math.Vector3;
 import com.projectdgdx.game.gameobjects.GameObject;
+import com.projectdgdx.game.gameobjects.Machine;
 
 /**
  * Created by Hampus on 2017-03-26.
@@ -9,28 +11,24 @@ public class GameObjectInit {
     private String type;
 
     //Different values with defaults:
-    private double x = 0;
-    private double y = 0;
-    private double z = 0;
+    private float x = 0;
+    private float y = 0;
+    private float z = 0;
 
-    private double scaleX = 1;
-    private double scaleY = 1;
-    private double scaleZ = 1;
+    private float scaleX = 1;
+    private float scaleY = 1;
+    private float scaleZ = 1;
 
-    private double width = 1;
-    private double height  = 1;
-    private double depth = 1;
-
-    private double rotationX = 0;
-    private double rotationY = 0;
-    private double rotationZ = 0;
+    private float rotationX = 0;
+    private float rotationY = 0;
+    private float rotationZ = 0;
 
     private Polygon polygon = null;
 
-    private double spawnRate = 1;
+    private float spawnRate = 1;
     boolean spawnRateRandom = false;
-    private double spawnDelay = 0;
-    private double aliveLimit = 10;
+    private float spawnDelay = 0;
+    private float aliveLimit = 10;
 
     public GameObjectInit(String type) {
         this.type = type;
@@ -38,14 +36,8 @@ public class GameObjectInit {
 
     public GameObject convert() {
         switch (type) {
-            case "Model":
-//                Class<?> clazz = Class.forName(className);
-//                GameObject date = clazz.newInstance();
-                return new GameObject(new Vector3(x, y, z), new Vector3(scaleX, scaleY, scaleZ), new Vector3(width, height, depth), new Vector3(rotationX, rotationY, rotationZ), "Model");
             case "Machine":
-                return new GameObject(new Vector3(x, y, z), new Vector3(scaleX, scaleY, scaleZ), new Vector3(width, height, depth), new Vector3(rotationX, rotationY, rotationZ), "machine.basic");
-            case "Spawn":
-                return new GameObject(new Vector3(x, y, z), new Vector3(scaleX, scaleY, scaleZ), new Vector3(width, height, depth), new Vector3(rotationX, rotationY, rotationZ), "Spawn");
+                return new Machine(new Vector3(x, y, z), new Vector3(scaleX, scaleY, scaleZ), new Vector3(rotationX, rotationY, rotationZ), "machine.basic");
             default:
                 System.out.println("TAG OF TYPE: " + type + " not supported");
                 return null;
@@ -55,40 +47,31 @@ public class GameObjectInit {
     public void changeValue(String key, String value) {
         switch(key) {
             case "x":
-                x = Double.parseDouble(value);
+                x = Float.parseFloat(value);
                 break;
             case "y":
-                y = Double.parseDouble(value);
+                y = Float.parseFloat(value);
                 break;
             case "z":
-                z = Double.parseDouble(value);
+                z = Float.parseFloat(value);
                 break;
             case "scaleX":
-                scaleX = Double.parseDouble(value);
+                scaleX = Float.parseFloat(value);
                 break;
             case "scaleY":
-                scaleY = Double.parseDouble(value);
+                scaleY = Float.parseFloat(value);
                 break;
             case "scaleZ":
-                scaleZ = Double.parseDouble(value);
-                break;
-            case "width":
-                width = Double.parseDouble(value);
-                break;
-            case "height":
-                height = Double.parseDouble(value);
-                break;
-            case "depth":
-                depth = Double.parseDouble(value);
+                scaleZ = Float.parseFloat(value);
                 break;
             case "rotationX":
-                rotationX = Double.parseDouble(value);
+                rotationX = Float.parseFloat(value);
                 break;
             case "rotationY":
-                rotationY = Double.parseDouble(value);
+                rotationY = Float.parseFloat(value);
                 break;
             case "rotationZ":
-                rotationZ = Double.parseDouble(value);
+                rotationZ = Float.parseFloat(value);
                 break;
             case "polygon":
                 // TODO Handle polygons
