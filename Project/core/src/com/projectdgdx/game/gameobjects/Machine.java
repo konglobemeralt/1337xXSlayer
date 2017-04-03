c5179479c32792841c1d3f8aec98710c2db2fe97
Zumpo <carlsson889@gmail.com>
2017-04-03 10:35:47 +0200

    Added machine class

---
+++ Project/core/src/com/projectdgdx/game/gameobjects/Machine.java
@@ -0,0 +1,12 @@
+package com.projectdgdx.game.gameobjects;
+
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by Hampus on 2017-04-03.
+ */
+public class Machine extends StaticObject {
+    public Machine(Vector3 position, Vector3 scale, Vector3 rotation, String id) {
+        super(position, scale, rotation, id);
+    }
+}
