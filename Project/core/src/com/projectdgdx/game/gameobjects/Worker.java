79ee3b1ed7d5982b6fcc86e7ed1e5a02e4789ecc
Zumpo <carlsson889@gmail.com>
2017-04-03 10:20:15 +0200

    Added worker and player class

---
+++ Project/core/src/com/projectdgdx/game/gameobjects/Worker.java
@@ -0,0 +1,12 @@
+package com.projectdgdx.game.gameobjects;
+
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by Hampus on 2017-04-03.
+ */
+public class Worker extends Character {
+    public Worker(Vector3 position, Vector3 scale, Vector3 size, Vector3 rotation, String id) {
+        super(position, size, scale, rotation, id);
+    }
+}
