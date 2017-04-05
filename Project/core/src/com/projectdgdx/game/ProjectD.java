94df1d4c8b003c24009182886decd099d1a53ca2
Konglobemeralt <jesper.blidkvist@live.se>
2017-04-05 11:07:27 +0200

    shader test code, dummy sphere without env variables

--- Project/core/src/com/projectdgdx/game/ProjectD.java
+++ Project/core/src/com/projectdgdx/game/ProjectD.java
@@ -2,157 +2,46 @@ package com.projectdgdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g3d.*;
-import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
-import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.model.NodePart;
 import com.badlogic.gdx.graphics.g3d.shaders.DefaultShader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.DefaultTextureBinder;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.RenderContext;
-import com.badlogic.gdx.math.Vector3;
-import com.projectdgdx.game.gameobjects.GameObject;
-import com.projectdgdx.game.utils.AssetManager;
-import com.projectdgdx.game.utils.AssetsFinder;
-import com.projectdgdx.game.utils.Map;
 
-import com.badlogic.gdx.utils.Array;
-import com.projectdgdx.game.utils.MapParser;
-
-import java.util.Random;
 
 public class ProjectD extends ApplicationAdapter {
-    private PerspectiveCamera cam;
-    private CameraInputController camController;
-    private ModelBatch modelBatch;
-    public Array<ModelInstance> instances = new Array<ModelInstance>();
-    public Environment environment;
-    public boolean loading;
-
-    public Texture roboTexture;
-
-    public Renderable renderable;
-    public RenderContext renderContext;
+    public PerspectiveCamera cam;
+    public CameraInputController camController;
     public Shader shader;
-
-    Random rand;
-    Map map;
+    public RenderContext renderContext;
+    public Model model;
+    public Renderable renderable;
 
     @Override
     public void create () {
-        MapParser parser = new MapParser();
-        map = parser.parse("BasicMap");
-        rand = new Random();
-        loadAssets();
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(2f, 2f, 2f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
 
-        createEnvironment();
-        createCamera();
-
-
-    }
-
-    public void loadAssets(){
-        modelBatch = new ModelBatch();
-        //model
-        AssetManager.loadModel("robo.g3dj");
-        AssetManager.loadModel("machine.g3dj");
-        AssetManager.loadModel("ship.g3db");
-
-        loading = true;
-    }
-
-    private void doneLoading() {
-
-        AssetManager.setTextureToModel("copper.jpg", "robo.g3dj");
-        AssetManager.setTextureToModel("metal.jpg", "machine.g3dj");
-
-
-        ModelInstance playerInstance;
-        playerInstance = new ModelInstance(AssetManager.getModel("robo.g3dj"));
-        playerInstance.transform.setToTranslation(0, 0, 0);
-        playerInstance.transform.scale(0.2f, 0.2f, 0.2f);
-
-
-        instances.add(playerInstance);
-
-        for (int x = 0; x < 150; x += 1) {
-            ModelInstance npcInstance;
-            npcInstance = new ModelInstance(AssetManager.getModel("ship.g3db"));
-            npcInstance.transform.setToTranslation(rand.nextFloat() * (50 - -50) + -50, 0, rand.nextFloat() * (50 - -50) + -50);
-            npcInstance.transform.scale(2f, 2f, 2f);
-            npcInstance.transform.rotate(Vector3.Y, rand.nextFloat() * 360f);
-
-            instances.add(npcInstance);
-        }
-
-        for (GameObject gameObject : map.getGameObjects()) {
-            ModelInstance npcInstance;
-            System.out.println(AssetsFinder.getModelPath(gameObject.getId()));
-            npcInstance = new ModelInstance(AssetManager.getModel(AssetsFinder.getModelPath(gameObject.getId())));
-            npcInstance.transform.setToTranslation(gameObject.getPosition());
-            Vector3 scale = gameObject.getScale();
-            npcInstance.transform.scale(scale.x, scale.y, scale.z);
-            Vector3 rotation = gameObject.getRotation();
-            npcInstance.transform.rotate(Vector3.X, rotation.x);
-            npcInstance.transform.rotate(Vector3.Y, rotation.y);
-            npcInstance.transform.rotate(Vector3.Z, rotation.z);
-
-            instances.add(npcInstance);
-        }
-
-
-
-        loading = false;
-    }
-
-    public void createEnvironment(){
-        environment = new Environment();
-        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
-    }
-
-    public void createCamera(){
-        cam = new PerspectiveCamera(75, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(0f, 0f, 3f);
-        cam.lookAt(0f, 0f, 0f);
-        cam.near = 0.01f;
-        cam.far = 1000f;
         camController = new CameraInputController(cam);
         Gdx.input.setInputProcessor(camController);
 
-    }
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createSphere(2f, 2f, 2f, 20, 20,
+                new Material(),
+                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal | VertexAttributes.Usage.TextureCoordinates);
 
-
-    public void render () {
-        if (loading && AssetManager.update())
-            doneLoading();
-        cam.update();
-
-
-        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-
-
-        if(!loading) {
-            moveModel(instances.get(0));
-            setShader(instances.get(0));
-        }
-
-
-        modelBatch.begin(cam);
-        modelBatch.render(instances, environment);
-        modelBatch.end();
-    }
-
-    private void setShader(ModelInstance instance){
-        NodePart rootNode = instance.nodes.get(0).parts.get(0);
+        NodePart blockPart = model.nodes.get(0).parts.get(0);
 
         renderable = new Renderable();
-        rootNode.setRenderable(renderable);
+        blockPart.setRenderable(renderable);
         renderable.environment = null;
-
         renderable.worldTransform.idt();
 
         renderContext = new RenderContext(new DefaultTextureBinder(DefaultTextureBinder.WEIGHTED, 1));
@@ -160,31 +49,35 @@ public class ProjectD extends ApplicationAdapter {
         shader.init();
     }
 
-    private void moveModel(ModelInstance instance){
+    @Override
+    public void render () {
+        camController.update();
 
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
-            instance.transform.trn(1f, 0, 0);
-        }
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
-            instance.transform.trn(-1f, 0, 0);
-        }
-
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
-            instance.transform.trn(0, 0, -1f);
-        }
-
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
-            instance.transform.trn(0, 0, 1f);
-        }
-
-
+        renderContext.begin();
+        shader.begin(cam, renderContext);
+        shader.render(renderable);
+        shader.end();
+        renderContext.end();
     }
 
     @Override
     public void dispose () {
-        modelBatch.dispose();
-        instances.clear();
-        AssetManager.dispose();
+        shader.dispose();
+        model.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
     }
 }
