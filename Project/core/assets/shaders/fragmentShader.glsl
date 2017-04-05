09b73f717893ad5354efc7860fb59482796fa7b1
Konglobemeralt <jesper.blidkvist@live.se>
2017-04-05 11:29:27 +0200

    basic shader working. Can add a color tint

--- Project/core/assets/shaders/fragmentShader.glsl
+++ Project/core/assets/shaders/fragmentShader.glsl
@@ -4,7 +4,13 @@ precision mediump float;
 
 varying vec2 v_texCoord0;
 
+uniform vec3 u_color;
+uniform sampler2D u_texture;
+
 void main() {
-    vec4 colorTint = vec4(0.7, 0.1, 0.6, 1.0);
-    gl_FragColor = vec4(v_texCoord0, 0.0, 1.0) + colorTint;
-}
+    vec4 pink = vec4(0.7, 0.1, 0.7, 1.0);
+    vec4 colorTint = vec4(v_texCoord0.x/2.0, v_texCoord0.y/2.0, 0, 1.0);
+    colorTint = colorTint * pink;
+    gl_FragColor = texture2D(u_texture, v_texCoord0) + colorTint;
+    //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
