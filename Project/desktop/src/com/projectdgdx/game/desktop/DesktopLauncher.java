a9eac1b275bfbc6e9a1d0f4054e254bd1c1ab052
Konglobemeralt <jesper.blidkvist@live.se>
2017-04-06 11:44:42 +0200

    configured AA

--- Project/desktop/src/com/projectdgdx/game/desktop/DesktopLauncher.java
+++ Project/desktop/src/com/projectdgdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import com.projectdgdx.game.ProjectD;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.samples = 8;
 		new LwjglApplication(new ProjectD(), config);
 	}
 }
