c6e9df3754fb81dc79b83fa2881bd2c138c68edf
Konglobemeralt <jesper.blidkvist@live.se>
2017-04-06 13:44:04 +0200

    nice shading with UV color

--- Project/core/assets/shaders/fragmentShader.glsl
+++ Project/core/assets/shaders/fragmentShader.glsl
@@ -5,6 +5,6 @@ precision mediump float;
 varying vec2 v_texCoord0;
 
 void main() {
-    vec4 colorTint = vec4(0.5, 0.1, 0.1, 1.0);
+    vec4 colorTint = vec4(0.7, 0.1, 0.6, 1.0);
     gl_FragColor = vec4(v_texCoord0, 0.0, 1.0) + colorTint;
 }
