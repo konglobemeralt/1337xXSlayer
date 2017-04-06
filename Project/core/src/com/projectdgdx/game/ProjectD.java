9d170343dff0ebe749c38b444c8ea6beb4d677e5
Konglobemeralt <jesper.blidkvist@live.se>
2017-04-06 14:39:40 +0200

    flashing light shader code

--- Project/core/src/com/projectdgdx/game/ProjectD.java
+++ Project/core/src/com/projectdgdx/game/ProjectD.java
@@ -10,8 +10,10 @@ import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.DefaultTextureBinder;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.graphics.g3d.utils.RenderContext;
+import com.badlogic.gdx.utils.Array;
 import com.projectdgdx.game.renderer.BaseShader;
 import com.projectdgdx.game.utils.AssetManager;
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
 
 
 public class ProjectD extends ApplicationAdapter {
@@ -20,6 +22,9 @@ public class ProjectD extends ApplicationAdapter {
     public Shader shader;
     public RenderContext renderContext;
     public Model model;
+    public Array<ModelInstance> instances = new Array<ModelInstance>();
+    public ModelBatch modelBatch;
+
     public Renderable renderable;
 
     @Override
@@ -28,7 +33,7 @@ public class ProjectD extends ApplicationAdapter {
         cam.position.set(2f, 100f, 150f);
         cam.lookAt(0,0,0);
         cam.near = 1f;
-        cam.far = 300f;
+        cam.far = 10000f;
         cam.update();
 
         camController = new CameraInputController(cam);
@@ -37,6 +42,12 @@ public class ProjectD extends ApplicationAdapter {
         AssetManager.loadModel("robo.g3dj");
 
         Model model = AssetManager.getRawModel("robo.g3dj");
+
+        for (int x = -500; x <= 500; x+=60) {
+            for (int z = -500; z<=500; z+=60) {
+                instances.add(new ModelInstance(model, x, 0, z));
+            }
+        }
 
         NodePart blockPart = model.getNode("robo_root").getChild(0).parts.get(0);
 
@@ -48,6 +59,8 @@ public class ProjectD extends ApplicationAdapter {
         renderContext = new RenderContext(new DefaultTextureBinder(DefaultTextureBinder.WEIGHTED, 1));
         shader = new BaseShader();
         shader.init();
+
+        modelBatch = new ModelBatch();
     }
 
     @Override
@@ -60,15 +73,16 @@ public class ProjectD extends ApplicationAdapter {
                 (Gdx.graphics.getBufferFormat().coverageSampling?GL20.GL_COVERAGE_BUFFER_BIT_NV:0));
         //renderable.meshPart.primitiveType = GL20.GL_LINE_STRIP;
 
-        renderContext.begin();
-        shader.begin(cam, renderContext);
-        shader.render(renderable);
-        shader.end();
-        renderContext.end();
+        modelBatch.begin(cam);
+        for (ModelInstance instance : instances)
+            modelBatch.render(instance, shader);
+        modelBatch.end();
     }
 
     @Override
     public void dispose () {
+        AssetManager.dispose();
+        modelBatch.dispose();
         shader.dispose();
         model.dispose();
     }
