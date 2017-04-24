bea80dfa5d7209d75817ee8e68fb62445259a21e
Konglobemeralt <jesper.blidkvist@live.se>
2017-04-24 14:46:48 +0200

    Added window size to config in DesktopLauncher

--- Project/desktop/src/com/projectdgdx/game/desktop/DesktopLauncher.java
+++ Project/desktop/src/com/projectdgdx/game/desktop/DesktopLauncher.java
@@ -4,9 +4,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.projectdgdx.game.ProjectD;
 
+
+
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1280;
+		config.height = 720;
+		config.resizable = false;
 		config.samples = 8;
 		new LwjglApplication(new ProjectD(), config);
 	}
