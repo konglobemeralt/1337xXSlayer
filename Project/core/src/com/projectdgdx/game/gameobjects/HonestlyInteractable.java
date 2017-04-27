1905cd51b649cfe3d247257173f084bf76f409e8
Emil Jansson <jaemil@student.chalmers.se>
2017-04-27 14:51:32 +0200

    Completed committing some files that schould have been in the last commit.

---
+++ Project/core/src/com/projectdgdx/game/gameobjects/HonestlyInteractable.java
@@ -0,0 +1,11 @@
+package com.projectdgdx.game.gameobjects;
+
+import com.badlogic.gdx.math.Vector3;
+
+/**
+ * Created by Emil Jansson on 2017-04-25.
+ */
+public interface HonestlyInteractable {
+    public abstract void honsestInteract();
+    public abstract Vector3 getPosition();
+}
---
+++ Project/core/src/com/projectdgdx/game/gameobjects/SpotlightPlayerState.java
@@ -0,0 +1,11 @@
+package com.projectdgdx.game.gameobjects;
+
+/**
+ * Created by Emil Jansson on 2017-04-25.
+ */
+public class SpotlightPlayerState implements CharacterState {
+    @Override
+    public void move() { //TODO
+
+    }
+}
