cf995434b87cc16fbd7db7633174f2adcb1738e9
Emil Jansson <jaemil@student.chalmers.se>
2017-04-27 14:46:56 +0200

    Created new state interface: CharacterState. Created two states: NormalPlayerState and SpotlightPlayerState. Created Interfaces for interactable objects: DishonestlyInteractable and HonestlyInteractable. Implemented some functionality in Character, Entity, Machine and PlayableCharacter.

--- Project/core/src/com/projectdgdx/game/gameobjects/Character.java
+++ Project/core/src/com/projectdgdx/game/gameobjects/Character.java
@@ -7,7 +7,18 @@ import com.badlogic.gdx.math.Vector3;
  * Created by Hampus on 2017-04-03.
  */
 public abstract class Character extends Entity {
+
+    protected CharacterState state;
+
     public Character(Vector3 position, Vector3 scale, Vector3 rotation, String id) {
         super(position, scale, rotation, id);
+        setStartingState();
     }
+
+    /**
+     * Sets the starting state of a character
+     */
+
+    protected abstract void setStartingState();
+
 }
--- Project/core/src/com/projectdgdx/game/gameobjects/Machine.java
+++ Project/core/src/com/projectdgdx/game/gameobjects/Machine.java
@@ -5,8 +5,13 @@ import com.badlogic.gdx.math.Vector3;
 /**
  * Created by Hampus on 2017-04-03.
  */
-public class Machine extends StaticObject {
+public class Machine extends StaticObject implements HonestlyInteractable{
     public Machine(Vector3 position, Vector3 scale, Vector3 rotation, String id) {
         super(position, scale, rotation, id);
+    }
+
+    @Override
+    public void honsestInteract() { //TODO
+
     }
 }
