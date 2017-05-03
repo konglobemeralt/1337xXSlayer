04ae57ff2a28a75a5da74dc17ea63b60ab51f76d
Emil Jansson <emiljanQ3@hotmail.com>
2017-05-03 10:11:02 +0200

    Created machine states.

--- Project/core/src/com/projectdgdx/game/model/Machine.java
+++ Project/core/src/com/projectdgdx/game/model/Machine.java
@@ -5,14 +5,26 @@ import com.badlogic.gdx.math.Vector3;
 /**
  * Created by Hampus on 2017-04-03.
  */
+
 public class Machine extends StaticObject implements HonestInteractable {
+
+
+    protected MachineState state;
+
     public Machine(Vector3 position, Vector3 scale, Vector3 rotation, String id) {
         super(position, scale, rotation, id);
+        this.state = new UnusedMachine();
     }
 //w
     @Override
-    public void honestInteract(PlayableCharacter player){
 
+    public void honestInteract(PlayableCharacter player) { //TODO  test commit
+        state.honestInteract(player);
+    }
+
+
+    public void dishonestInteract(PlayableCharacter player) {
+        state.dishonestInteract(player);
     }
 
 }
---
+++ Project/core/src/com/projectdgdx/game/model/UnusedMachine.java
@@ -0,0 +1,17 @@
+package com.projectdgdx.game.model;
+
+/**
+ * Created by Emil Jansson on 2017-05-03.
+ */
+public class UnusedMachine implements MachineState {
+
+    @Override
+    public void honestInteract(PlayableCharacter player) {
+
+    }
+
+    @Override
+    public void dishonestInteract(PlayableCharacter player) {
+
+    }
+}
