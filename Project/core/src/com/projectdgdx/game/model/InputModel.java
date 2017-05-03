7e6c9f4fb6c6feeaad81d5cf6c51f9b010d03c43
Zumpo <carlsson889@gmail.com>
2017-05-03 10:35:35 +0200

    GamePad buttons will now be registered

--- Project/core/src/com/projectdgdx/game/model/InputModel.java
+++ Project/core/src/com/projectdgdx/game/model/InputModel.java
@@ -7,10 +7,46 @@ import com.projectdgdx.game.utils.Vector2d;
  */
 public class InputModel {
     private Vector2d leftStick = new Vector2d(0,0);
+    private PressedData buttonA = new PressedData();
+    private PressedData buttonB = new PressedData();
+    private PressedData buttonX = new PressedData();
+    private PressedData buttonY = new PressedData();
 
-    //This method is not pure.
     public Vector2d getLeftStick() {
         return leftStick;
+    }
+
+    public void setButtonA(boolean isPressed) {
+        buttonA.setPressed(isPressed);
+    }
+    public void setButtonB(boolean isPressed) {
+        buttonB.setPressed(isPressed);
+    }
+    public void setButtonX(boolean isPressed) {
+        buttonX.setPressed(isPressed);
+    }
+    public void setButtonY(boolean isPressed) {
+        buttonY.setPressed(isPressed);
+    }
+
+    public PressedData getButtonA() {
+        return buttonA;
+    }
+    public PressedData setButtonB() {
+        return buttonB;
+    }
+    public PressedData setButtonX() {
+        return buttonX;
+    }
+    public PressedData setButtonY() {
+        return buttonY;
+    }
+
+    public void resetButtonCounts() {
+        buttonA.resetCount();
+        buttonB.resetCount();
+        buttonX.resetCount();
+        buttonY.resetCount();
     }
 
 }
