bb3030380b6103ef0ffe9bb43b060e612f58f7c3
Emil Jansson <emiljanQ3@hotmail.com>
2017-05-04 14:45:02 +0200

    Started implementing timer into interacting playerstate.

--- Project/core/src/com/projectdgdx/game/model/MachineInteractingPlayerState.java
+++ Project/core/src/com/projectdgdx/game/model/MachineInteractingPlayerState.java
@@ -5,15 +5,22 @@ import com.projectdgdx.game.utils.Vector3d;
 /**
  * Created by Emil Jansson on 2017-05-04.
  */
-public class MachineInteractingPlayerState implements CharacterState {
+public class MachineInteractingPlayerState implements CharacterState, TimerListener {
     private HonestInteractable machine;
 
     MachineInteractingPlayerState(HonestInteractable machine){
         this.machine = machine;
+        Timer timer = new Timer(5, 1000);
+        timer.addListener(this);
         //TODO start timer that releases player after set amount of time
     }
     @Override
     public void move(Vector3d vector) {
         //Player is stationary during machine interaction
+    }
+
+    @Override
+    public void timeIsUp() { //TODO
+
     }
 }
