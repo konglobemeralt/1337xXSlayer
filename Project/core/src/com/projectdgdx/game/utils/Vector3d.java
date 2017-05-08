25b3b692192b403bf73b96297e615bd77266b854
sk8eddies <sk8eddie97@gmail.com>
2017-05-08 13:55:31 +0200

    FIXED: DistanceTo method in vetor and its usage in palyer character.

--- Project/core/src/com/projectdgdx/game/utils/Vector3d.java
+++ Project/core/src/com/projectdgdx/game/utils/Vector3d.java
@@ -25,4 +25,9 @@ public class Vector3d {
         return (float)Math.sqrt(x*x + y*y + z*z);
     }
 
+    public float distanceTo(Vector3d compVector){
+        Vector3d distanceVector = new Vector3d(compVector.x-x, compVector.y-y, compVector.z-z);
+        return distanceVector.getLength();
+    }
+
 }
