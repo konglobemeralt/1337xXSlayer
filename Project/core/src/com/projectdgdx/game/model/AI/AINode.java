c62365a9b549f2b83bfc454bf67fd8b6dc92e6a4
Emil Jansson <emiljanQ3@hotmail.com>
2017-05-08 11:40:36 +0200

    Created and implemented AINode class. Created shells for WorkerNode and SpotlightNode.

---
+++ Project/core/src/com/projectdgdx/game/model/AI/AINode.java
@@ -0,0 +1,53 @@
+package com.projectdgdx.game.model.AI;
+
+import com.projectdgdx.game.model.GameObject;
+import com.projectdgdx.game.utils.Vector3d;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Emil Jansson on 2017-05-08.
+ */
+public abstract class AINode extends GameObject {
+
+    private ArrayList<AINode> connectingNodes = new ArrayList<AINode>();
+    private ArrayList<Double> connectionStrengths = new ArrayList<Double>();
+
+    public AINode(Vector3d pos){
+        super(pos,null,null, "AINode");
+    }
+
+    public void addConnection(AINode node, double strength){
+        connectingNodes.add(node);
+        connectionStrengths.add(strength);
+    }
+
+    public void setConnections(AINode[] nodes, double[] strengths){
+        connectingNodes.clear();
+        connectionStrengths.clear();
+        int i = 0;
+        for (AINode node : nodes) {
+            connectingNodes.add(node);
+            connectionStrengths.add(strengths[i]);
+            i++;
+        }
+    }
+
+    public AINode getNextNode(){ //Returns a node based on connection strength.
+        double strenghtSum = 0;
+        for (double d : connectionStrengths){
+            strenghtSum += d;
+        }
+        double threshold = strenghtSum*Math.random();
+        strenghtSum = 0;
+        int i = 0;
+        for (double d : connectionStrengths){
+            strenghtSum += d;
+            if (strenghtSum > threshold){
+                break;
+            }
+        }
+        return connectingNodes.get(i);
+    }
+
+}
---
+++ Project/core/src/com/projectdgdx/game/model/AI/SpotlightNode.java
@@ -0,0 +1,7 @@
+package com.projectdgdx.game.model.AI;
+
+/**
+ * Created by Emil Jansson on 2017-05-08.
+ */
+public class SpotlightNode {
+}
