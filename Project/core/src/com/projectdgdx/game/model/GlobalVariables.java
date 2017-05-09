c2cac41e94d2f62eb594ec163d80ff9e9c8e8305
Emil Jansson <emiljanQ3@hotmail.com>
2017-05-09 15:10:59 +0200

    Added worker states and began implementation of StrikingWorkerState.

--- Project/core/src/com/projectdgdx/game/model/GlobalVariables.java
+++ Project/core/src/com/projectdgdx/game/model/GlobalVariables.java
@@ -4,4 +4,6 @@ package com.projectdgdx.game.model;
  * Created by Emil Jansson on 2017-04-25.
  */
 public class GlobalVariables {
+    public static final float machineActDistance = 1;
+    public static final float workerSpeed = 1;
 }
