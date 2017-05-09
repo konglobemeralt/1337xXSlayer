22c46c11797e3af0c55812610927d9574f58e1af
Zumpo <carlsson889@gmail.com>
2017-05-09 16:11:55 +0200

    Now checking for collisions. Not working yet however

--- Project/core/src/com/projectdgdx/game/controller/InGameState.java
+++ Project/core/src/com/projectdgdx/game/controller/InGameState.java
@@ -15,9 +15,7 @@ import com.badlogic.gdx.utils.Array;
 import com.projectdgdx.game.Config;
 import com.projectdgdx.game.GameStates;
 import com.projectdgdx.game.ProjectD;
-import com.projectdgdx.game.model.GameObject;
-import com.projectdgdx.game.model.InputModel;
-import com.projectdgdx.game.model.PlayableCharacter;
+import com.projectdgdx.game.model.*;
 import com.projectdgdx.game.utils.*;
 import com.projectdgdx.game.utils.Map;
 import com.projectdgdx.game.view.RenderManager;
@@ -159,18 +157,15 @@ public class InGameState implements GameState {
             //Add a box around object that will be used for physics
 			BoundingBox boundingBox = modelInstance.model.calculateBoundingBox(new BoundingBox());
             System.out.println(boundingBox.getDimensions(new Vector3()).toString());
+
 			btCollisionShape collisionShape = new btBoxShape(boundingBox.getDimensions(new Vector3()));
+//			btCollisionShape collisionShape = new btBoxShape(new Vector3(100f, 100f, 100f));
 			btCollisionObject collisionObject = new btCollisionObject();
 			collisionObject.setCollisionShape(collisionShape);
 			collisionObject.setWorldTransform(modelInstance.transform);
 
-
-
-
-
 			//Add GameObject and ModelInstance to a map that keeps them together
             objectsMap.put(gameObject, new Pair<ModelInstance, btCollisionObject>(modelInstance, collisionObject));
-
         }
 
     }
@@ -189,20 +184,38 @@ public class InGameState implements GameState {
         for(java.util.Map.Entry<GameObject, Pair<ModelInstance, btCollisionObject>> entrySet : objectsMap.entrySet()) {
             ModelInstance modelInstance = entrySet.getValue().getKey();
             GameObject gameObject = entrySet.getKey();
-            Vector3 position = VectorConverter.convertToLibgdx(gameObject.getPosition());
-            Vector3 scale = VectorConverter.convertToLibgdx(gameObject.getScale());
-            Quaternion quaternion = new Quaternion();
+            btCollisionObject collisionObject = entrySet.getValue().getValue();
 
-            //TODO ugly solution for rotation
-			Vector3 rotation = VectorConverter.convertToLibgdx(gameObject.getRotation());
-			modelInstance.transform.setToRotation(0,0,0,0,0,0);
-			modelInstance.transform.rotate(Vector3.X, rotation.x);
-			modelInstance.transform.rotate(Vector3.Y, rotation.y);
-			modelInstance.transform.rotate(Vector3.Z, rotation.z);
-			quaternion = modelInstance.transform.getRotation(new Quaternion());
+            //Check for entity
+            if(gameObject instanceof Entity) {
+				Vector3 position = VectorConverter.convertToLibgdx(gameObject.getPosition());
+				Vector3 scale = VectorConverter.convertToLibgdx(gameObject.getScale());
+				Quaternion quaternion = new Quaternion();
 
-			Matrix4 matrix4 = new Matrix4(position, quaternion, scale);
-            modelInstance.transform.set(matrix4);
+				//TODO ugly solution for rotation
+				Vector3 rotation = VectorConverter.convertToLibgdx(gameObject.getRotation());
+				modelInstance.transform.setToRotation(0,0,0,0,0,0);
+				modelInstance.transform.rotate(Vector3.X, rotation.x);
+				modelInstance.transform.rotate(Vector3.Y, rotation.y);
+				modelInstance.transform.rotate(Vector3.Z, rotation.z);
+				quaternion = modelInstance.transform.getRotation(new Quaternion());
+
+				Matrix4 matrix4 = new Matrix4(position, quaternion, scale);
+				modelInstance.transform.set(matrix4);
+				collisionObject.setWorldTransform(modelInstance.transform);
+
+
+				//Check collisions
+				if(gameObject instanceof PlayableCharacter) {
+					for(StaticObject staticObject : map.getStaticObjects()) {
+						if(checkCollision(collisionObject, objectsMap.get(staticObject).getValue())) {
+							System.out.println("COLLIISISISONN!!");
+						}
+//							System.out.println(checkCollision(collisionObject, objectsMap.get(staticObject).getValue()));
+					}
+				}
+
+			}
         }
     }
 
@@ -228,6 +241,7 @@ public class InGameState implements GameState {
 	boolean checkCollision(btCollisionObject object0, btCollisionObject object1) {
 		CollisionObjectWrapper co0 = new CollisionObjectWrapper(object0);
 		CollisionObjectWrapper co1 = new CollisionObjectWrapper(object1);
+		System.out.println(object0.getWorldTransform().getTranslation(new Vector3()).toString() + "   " + object1.getWorldTransform().getTranslation(new Vector3()).toString());
 
 		btCollisionAlgorithmConstructionInfo ci = new btCollisionAlgorithmConstructionInfo();
 		ci.setDispatcher1(dispatcher);
