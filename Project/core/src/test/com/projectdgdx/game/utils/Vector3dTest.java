40284f2ac2122b9ef2a95d0f0124dd424ee5e1f1
Emil Jansson <emiljanQ3@hotmail.com>
2017-05-10 10:56:08 +0200

    TESTED Vector3d fully by adding 3 new tests.

--- Project/core/src/test/com/projectdgdx/game/utils/Vector3dTest.java
+++ Project/core/src/test/com/projectdgdx/game/utils/Vector3dTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -40,5 +41,41 @@ public class Vector3dTest {
         int distance = 1;
         assertTrue(Math.abs(vector.distanceTo(distVec)-distance) < 0.000001);
 
+    }
+
+    @Test
+    public void testGetLength(){
+        // Verify that the length returned is right according to pythagoras theorem.
+        Vector3d testVector = new Vector3d(23,86,52);
+        assertEquals((float) Math.sqrt(23f*23f + 86f*86f + 52f*52f), testVector.getLength(), 0.000001);
+    }
+
+    @Test
+    public void testNormalised(){
+        // Verify that the normalised vectors length is equal to 1.
+        Vector3d testVector = new Vector3d(54,23,57);
+        assertEquals(1f, testVector.normalised().getLength(), 0.000001);
+    }
+
+    @Test
+    public void testVectorTo(){
+        // TODO vet ej vad skriva
+        Vector3d testVector1 = new Vector3d(74,82,17);
+        Vector3d testVector2 = new Vector3d(-13, 52, -64);
+        Vector3d Vector1to2  = testVector1.vectorTo(testVector2);
+        assertTrue(Math.abs(Vector1to2.x - (-13-74)) < 0.000001);
+        assertTrue(Math.abs(Vector1to2.y - (52-82)) < 0.000001);
+        assertTrue(Math.abs(Vector1to2.z - (-64-17)) < 0.000001);
+    }
+
+    @Test
+    public void testIsInRadius(){
+        // Verify that a vector inside the radius returns true while a vector outside of the radius returns false.
+        Vector3d testVector1 = new Vector3d(74,82,17);
+        Vector3d testVector2 = new Vector3d(-13, 52, -64);
+        Vector3d testVector3 = new Vector3d(-18, 52, -64);
+        float testRadius     = 16;
+        assertFalse(testVector1.isInRadius(testVector2, testRadius));
+        assertTrue(testVector2.isInRadius(testVector3, testRadius));
     }
 }
