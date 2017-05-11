bb35cffc155c0d6fe21ace277d38a1ab3b1a2ab6
Konglobemeralt <jesper.blidkvist@live.se>
2017-05-11 15:57:03 +0200

    Added documentation to Entity, PlayableCharater, Spotlight and MapParser

--- Project/core/src/com/projectdgdx/game/model/Entity.java
+++ Project/core/src/com/projectdgdx/game/model/Entity.java
@@ -13,20 +13,20 @@ public abstract class Entity extends GameObject {
 
     /**
      * Returns true if moving does not cause a collision.
+     * @param vec Vector3d
      * @return True if moving does not cause a collision.
      */
-
-    protected abstract boolean isColliding(Vector3d v);
+    protected abstract boolean isColliding(Vector3d vec);
 
     /**
-     * Changes the entity's position by translation by vector v.
-     * @param v The vector used for translation.
+     * Changes the entity's position by translation by vector vec.
+     * @param vec The vector used for translation.
      */
 
-    public void move(Vector3d v){
-        if (!isColliding(v)){
-            this.addPositionX(v.x);
-            this.addPositionZ(v.z);
+    public void move(Vector3d vec){
+        if (!isColliding(vec)){
+            this.addPositionX(vec.x);
+            this.addPositionZ(vec.z);
         }
 
     }
