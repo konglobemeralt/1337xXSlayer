6b8332df8f8a06320ac86128434dc4904433ab3e
konglobemeralt <jesper.blidkvist@live.se>
2017-05-12 13:56:04 +0200

    Some cleanup in menuStates

--- Project/core/src/com/projectdgdx/game/controller/MainMenuState.java
+++ Project/core/src/com/projectdgdx/game/controller/MainMenuState.java
@@ -33,41 +33,29 @@ public class MainMenuState implements iGameState {
 
     private SelectBox<Object> levelSelection;
     private List<String> levelList = new ArrayList<String>();
-    private String levelToLoad;
 
     private InputMultiplexer multiplexer;
     private Label mainMenuHeading;
     private Table table;
     private int controllerPosition = 0;
 
-    /**
-     * createBasicSkin reads an internalGUI file and creates a skin
-     *
-     * @param path Path to the GUIskin file
-     * @return A new skin
-     */
-    private Skin createBasicSkin(String path){
+    @Override
+    public void init(ProjectD projectD) {
+        File f = new File("map/");
+        levelList = new ArrayList<String>(Arrays.asList(f.list()));
+    }
 
-        return new Skin(Gdx.files.internal(path));
-       // //Create a font
-       // BitmapFont font = new BitmapFont();
-       // skin = new Skin();
-       // skin.add("default", font);
-//
-       // //Create a texture
-       // Pixmap pixmap = new Pixmap((int) Gdx.graphics.getWidth()/4,(int)Gdx.graphics.getHeight()/10, Pixmap.Format.RGB888);
-       // pixmap.setColor(Color.WHITE);
-       // pixmap.fill();
-       // skin.add("background",new Texture(pixmap));
-//
-       // //Create a button style
-       // TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
-       // textButtonStyle.up = skin.newDrawable("background", Color.GRAY);
-       // textButtonStyle.down = skin.newDrawable("background", Color.DARK_GRAY);
-       // textButtonStyle.checked = skin.newDrawable("background", Color.DARK_GRAY);
-       // textButtonStyle.over = skin.newDrawable("background", Color.LIGHT_GRAY);
-       // textButtonStyle.font = skin.getFont("default");
-       // skin.add("default", textButtonStyle);
+    @Override
+    public void start(ProjectD projectD) {
+        buildMenu();
+
+        //Set to first selection as default
+        Config.LEVEL_IN_PLAY = ((Label) levelSelection.getSelected()).getText().toString();
+
+        this.multiplexer = projectD.getMultiplexer();
+        multiplexer.addProcessor(stage);// Make the stage consume events
+        Gdx.input.setInputProcessor(multiplexer);
+
     }
 
     @Override
@@ -96,7 +84,6 @@ public class MainMenuState implements iGameState {
             Gdx.app.exit();
         }
 
-
         //TODO more logic for handeling movement of controllers or keyboards in menus
         for(InputController inputController : projectD.getInpuControllers()) {
             InputModel inputModel = inputController.getModel();
@@ -120,73 +107,6 @@ public class MainMenuState implements iGameState {
     }
 
     @Override
-    public void init(ProjectD projectD) {
-
-        File f = new File("map/");
-        levelList = new ArrayList<String>(Arrays.asList(f.list()));
-
-    }
-
-    @Override
-    public void start(ProjectD projectD) {
-        this.stage = new Stage();
-
-        skin = createBasicSkin(Config.UI_SKIN_PATH);
-
-        mainMenuHeading = new Label("Project D", skin);
-
-        newGameButton = new TextButton("New game", skin); // Use the initialized skin
-
-        settingsButton = new TextButton("Settings", skin);
-
-        exitButton = new TextButton("Exit Game", skin);
-
-
-        // Set up the SelectionBox with content
-        Object[] blob = new Object[levelList.size()];
-        for(int i = 0; i < levelList.size(); i++){
-            blob[i] = new Label(levelList.get(i), skin);
-        }
-       //blob[0] = new Label("Some random text that", skin);
-       //blob[1] = new Label("isn't being displayed!", skin);
-        levelSelection = new SelectBox<Object>(skin);
-        levelSelection.setItems(blob);
-
-
-        table = new Table();
-
-        table.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        table.add(mainMenuHeading).expandX().height(60);
-        table.row();
-        table.add(newGameButton).expandX().width(600).height(60);
-        table.row();
-        table.add(levelSelection).expandX().width(500).height(50);;
-        table.row();
-        table.add(settingsButton).expandX().width(600).height(60);
-        table.row();
-        table.add(exitButton).expandX().width(600).height(60);
-        table.row();
-
-        stage.addActor(table);
-
-        levelSelection.addListener(new ChangeListener() {
-            @Override
-            public void changed(ChangeEvent event, Actor actor) {
-                System.out.println(((Label) levelSelection.getSelected()).getText());
-                Config.LEVEL_IN_PLAY = ((Label) levelSelection.getSelected()).getText().toString();
-            }
-        });
-        //Set to first selection as default
-        Config.LEVEL_IN_PLAY = ((Label) levelSelection.getSelected()).getText().toString();
-
-        this.multiplexer = projectD.getMultiplexer();
-        multiplexer.addProcessor(stage);// Make the stage consume events
-        Gdx.input.setInputProcessor(multiplexer);
-
-    }
-
-    @Override
     public void stop(ProjectD projectD) {
         stage.dispose();
     }
@@ -196,5 +116,61 @@ public class MainMenuState implements iGameState {
         //stage.dispose();
     }
 
+    private void buildMenu(){
 
+        this.stage = new Stage();
+        skin = createBasicSkin(Config.UI_SKIN_PATH);
+
+        //Create buttons
+        mainMenuHeading = new Label("Project D", skin);
+        newGameButton = new TextButton("New game", skin);
+        settingsButton = new TextButton("Settings", skin);
+        exitButton = new TextButton("Exit Game", skin);
+
+
+        //Set up the SelectionBox with content
+        Object[] blob = new Object[levelList.size()];
+        for(int i = 0; i < levelList.size(); i++){
+            blob[i] = new Label(levelList.get(i), skin);
+        }
+        levelSelection = new SelectBox<Object>(skin);
+        levelSelection.setItems(blob);
+
+        //Create a table to display everything in rows
+        table = new Table();
+        table.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        //add everything to table
+        table.add(mainMenuHeading).expandX().height(60);
+        table.row();
+        table.add(newGameButton).expandX().width(600).height(60);
+        table.row();
+        table.add(levelSelection).expandX().width(600).height(30);;
+        table.row();
+        table.add(settingsButton).expandX().width(600).height(60);
+        table.row();
+        table.add(exitButton).expandX().width(600).height(60);
+        table.row();
+
+        stage.addActor(table);
+
+        //add listeners
+        levelSelection.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                System.out.println(((Label) levelSelection.getSelected()).getText());
+                Config.LEVEL_IN_PLAY = ((Label) levelSelection.getSelected()).getText().toString();
+            }
+        });
+    }
+
+    /**
+     * createBasicSkin reads an internalGUI file and creates a skin
+     *
+     * @param path Path to the GUIskin file
+     * @return A new skin
+     */
+    private Skin createBasicSkin(String path){
+        return new Skin(Gdx.files.internal(path));
+    }
 }
