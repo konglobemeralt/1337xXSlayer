c42ed69944523353d22e517811ae0dc5d271cef5
Konglobemeralt <jesper.blidkvist@live.se>
2017-05-18 13:54:37 +0200

    Removed AI-Prototype from root

--- AI-prototype/src/ObserverOfWalkerModel.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Emil Jansson on 2017-04-03.
- */
-public interface ObserverOfWalkerModel {
-    public abstract void reactOnUpdate();
-    public abstract void setWalkerRadius(int i);
-    public abstract void setDoubleToIntFactor(int i);
-}
