7f594f6ec990d8e6e6908ae0d0b37740fcda2156
sk8eddies <sk8eddie97@gmail.com>
2017-05-24 15:37:40 +0200

    FIXED: Circular dependencies between playables and staticinteractables. Added private to som attributes in playerstates.

--- Project/core/src/com/projectdgdx/game/model/interaction/iDishonestInteractable.java
+++ Project/core/src/com/projectdgdx/game/model/interaction/iDishonestInteractable.java
@@ -2,7 +2,10 @@ package com.projectdgdx.game.model.interaction;
 
 import com.projectdgdx.game.model.machineStates.iMachineState;
 import com.projectdgdx.game.model.playables.PlayableCharacter;
+import com.projectdgdx.game.model.staticInteractable.Spotlight;
 import com.projectdgdx.game.utils.Vector3d;
+
+import java.util.List;
 
 /**
  * All objects that can be destroyed/sabotaged by the saboteur should implement this interface.
@@ -14,7 +17,7 @@ public interface iDishonestInteractable {
      */
     void dishonestInteract(PlayableCharacter player);
     Vector3d getPosition();
-
+    List<Spotlight> getBigDetectingSpotlights();
     /**
      * This method is used to change the machines internal state when sabotaged.
      * @param newState , the new machine state that should be set when sabotaged.
