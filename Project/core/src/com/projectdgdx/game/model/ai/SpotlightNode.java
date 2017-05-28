82954644c3ad801027e0c24b75f753cd9e44d392
Emil Jansson <emiljanQ3@hotmail.com>
2017-05-28 18:39:43 +0200

    REMOVED CLASS SpotlightNode due to it being unused in the current build.

--- Project/core/src/com/projectdgdx/game/model/ai/SpotlightNode.java
+++
@@ -1,8 +0,0 @@
-package com.projectdgdx.game.model.ai;
-
-/**
- * The SpotlightNode class is used for when the Worker is interacting with main Spotlight. It is nodes
- * to where the Worker can steer the main Spotlight.
- */
-public class SpotlightNode {
-}
